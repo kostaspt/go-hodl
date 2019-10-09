94f128e241d013f01fcc623ebd1c42673852ba97
Konstantinos Pittas <konstantinos.pittas@gmail.com>
2019-10-09 17:12:03 +0300

    Format code

--- ticker/ticker.go
+++ ticker/ticker.go
@@ -55,19 +55,19 @@ func (t *Ticker) UpdateData() {
 		change7d, _ := strconv.ParseFloat(item["percent_change_7d"].(string), 64)
 		marketCap, _ := strconv.ParseFloat(item["market_cap_usd"].(string), 64)
 		r := record{
-			Rank:  item["rank"].(string),
-			Coin:  item["symbol"].(string),
-			Price: price,
-			Change1H: change1h,
+			Rank:      item["rank"].(string),
+			Coin:      item["symbol"].(string),
+			Price:     price,
+			Change1H:  change1h,
 			Change24H: change24h,
-			Change7D: change7d,
+			Change7D:  change7d,
 			MarketCap: marketCap,
 		}
 		t.Records = append(t.Records, r)
 	}
 }
 
-func (t Ticker) callApi() io.ReadCloser  {
+func (t Ticker) callApi() io.ReadCloser {
 	client := &http.Client{Timeout: 60 * time.Second}
 	resp, err := client.Get(t.ApiUrl.String())
 	if err != nil {
